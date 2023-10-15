965a0e538b803bea936d162f2ae128ee5c9c0777
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-10-15 12:31:49 +0100

    API AND cors

--- server.js
+++ server.js
@@ -31,6 +31,13 @@ app.get("/api/config/paypal", (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
 });
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use("/uploads", express.static("/var/data/uploads"));
