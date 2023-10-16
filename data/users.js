7d29898cd6762d174dd6c53d71605e0f8e97c4bb
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-10-16 02:32:13 +0100

    Added another admin

--- data/users.js
+++ data/users.js
@@ -1,22 +1,28 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const users = [
   {
-    name: 'Admin User',
-    email: 'admin@email.com',
-    password: bcrypt.hashSync('123456', 10),
+    name: "Admin User",
+    email: "admin@email.com",
+    password: bcrypt.hashSync("123456", 10),
     isAdmin: true,
   },
   {
-    name: 'John Doe',
-    email: 'john@email.com',
-    password: bcrypt.hashSync('123456', 10),
+    name: "Admin Admin",
+    email: "admin@gmail.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "john@email.com",
+    password: bcrypt.hashSync("123456", 10),
     isAdmin: false,
   },
   {
-    name: 'Jane Doe',
-    email: 'jane@email.com',
-    password: bcrypt.hashSync('123456', 10),
+    name: "Jane Doe",
+    email: "jane@email.com",
+    password: bcrypt.hashSync("123456", 10),
     isAdmin: false,
   },
 ];
